b230c1a1ecfac4465210fa08e3a48b1cde9aed57
zRalphy <rafalmec@gmail.com>
2022-02-01 15:21:00 +0100

    Added DTOs to project

---
+++ com.pgs.todo/src/main/java/com/pgs/todo/model/projection/GroupReadModel.java
@@ -0,0 +1,35 @@
+package com.pgs.todo.model.projection;
+
+import com.pgs.todo.model.Task;
+import com.pgs.todo.model.TaskGroup;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class GroupReadModel {
+
+    private String description;
+    /**
+     * Deadline from the latest task in group.
+     */
+    private LocalDateTime deadline;
+    private Set<GroupTaskReadModel> tasks;
+
+    public GroupReadModel(TaskGroup source) {
+        description = source.getDescription();
+
+        source.getTasks().stream()
+                .map(Task::getDeadline)
+                .max(LocalDateTime::compareTo)
+                .ifPresent(date -> deadline = date);
+
+        tasks = source.getTasks().stream()
+                .map(GroupTaskReadModel::new)
+                .collect(Collectors.toSet());
+    }
+}
