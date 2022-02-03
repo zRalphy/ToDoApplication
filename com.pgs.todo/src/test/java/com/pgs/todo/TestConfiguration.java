16d15a4cb747392d433613c4f0c007bb8a596059
zRalphy <rafalmec@gmail.com>
2022-02-03 12:25:00 +0100

    Deleted unused imports

--- com.pgs.todo/src/test/java/com/pgs/todo/TestConfiguration.java
+++ com.pgs.todo/src/test/java/com/pgs/todo/TestConfiguration.java
@@ -4,7 +4,6 @@ import com.pgs.todo.model.Task;
 import com.pgs.todo.repository.TaskRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
