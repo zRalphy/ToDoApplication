454e21770e2f4ec5cc9ed30843393538d0cc36be
zRalphy <rafalmec@gmail.com>
2022-02-09 09:40:38 +0100

    Added: -actuator to project and test annotation @EnableAsync -@RequestMapping annotation in controllers -MvcConfiguration for LoggerFilter and LoggerInterceptor (to learn how it is work)

--- com.pgs.todo/src/main/java/com/pgs/todo/TodoAppApplication.java
+++ com.pgs.todo/src/main/java/com/pgs/todo/TodoAppApplication.java
@@ -3,10 +3,12 @@ package com.pgs.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import javax.validation.Validator;
 
+@EnableAsync
 @SpringBootApplication
 public class TodoAppApplication {
 
